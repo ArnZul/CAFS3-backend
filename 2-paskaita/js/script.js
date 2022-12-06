be737a00e577a20852a1c9e46e216e0d79099094
Arnas Zulonas <zulonas.arnas@outlook.com>
2022-12-06 19:19:14 +0200

    Antra paskaita

---
+++ 2-paskaita/js/script.js
@@ -0,0 +1,17 @@
+console.log("Java script has loaded...");
+
+let x = "";
+
+if (x <= 0) {
+  console.log("ne grupė");
+} else if (x === 1) {
+  console.log("solo");
+} else if (x === 2) {
+  console.log("duetas");
+} else if (x === 3) {
+  console.log("trio");
+} else if (x === 4) {
+  console.log("kvartetas");
+} else {
+  console.log("didelė grupė");
+}
