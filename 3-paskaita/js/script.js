91b723606ea14c3b0476aed36c8ac7a8726fc9f1
Arnas Zulonas <zulonas.arnas@outlook.com>
2022-12-07 18:52:36 +0200

    added classWork

--- 3-paskaita/js/script.js
+++ 3-paskaita/js/script.js
@@ -1,8 +1,25 @@
 alert("Java script has loaded...");
-
-function plusPlus(x, y) {
+// Namu darbai
+function plusPlusUsingFor(x, y) {
   for (let i = 0; i < y; i++) {
     x++;
   }
+  return x;
+}
+// klases darbai // ciklai
+function plusPlusUsingWhile(x, y) {
+  let i = 0;
+  while (i < y) {
+    i++;
+    x = x + 1;
+  }
+  return x;
+}
+function plusPlusUsingDoWhile(x, y) {
+  let i = 0;
+  do {
+    i++;
+    x += 1;
+  } while (i < y);
   return x;
 }
