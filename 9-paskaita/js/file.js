ad6306c0ddd05b07580804e064761d5a5b30cbc0
ArnZul <zulonas.arnas@outlook.com>
2022-12-21 21:54:11 +0200

    x

--- 9-paskaita/js/file.js
+++ 9-paskaita/js/file.js
@@ -1,7 +1,8 @@
-let fs = require("fs");
-let x = "text.txt";
-let y = "text inside";
-fs.writeFile(x, y, function (err) {
+const fs = require("fs");
+const arg = process.argv.slice(2);
+const title = arg[0];
+const content = arg[1];
+fs.writeFile(title, content, function (err) {
   if (err) throw err;
   console.log("File is create successfully.");
 });
